7f6b743182e5f3020b08175f9da2be8ef18a6ee0
1535489722@qq.com <tj199024>
2021-09-18 17:10:11 +0800

    删除

--- python/factory/spider/Scrapy/Scrapy/items.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-
-class ScrapyItem(scrapy.Item):
-    bookList = scrapy.Field()
-
-class imageSrcItem(scrapy.Item):
-    srcList = scrapy.Field()
--- python/factory/tornado/common/md5.py
+++
@@ -1,8 +0,0 @@
-import hashlib
- 
-def md5File(filename, blocksize=65536):
-    hash = hashlib.md5()
-    with open(filename, 'rb') as f:
-        for block in iter(lambda: f.read(blocksize), b''):
-            hash.update(block)
-    return hash.hexdigest()
